3497b64e941a55152b90edbc6e12d5b15cb5fa73
feffy380 <114889020+feffy380@users.noreply.github.com>
2025-01-22 15:05:03 +0100

    feat: timestep unified caching. only first model call is checked

--- scripts/teacache.py
+++ scripts/teacache.py
@@ -23,14 +23,16 @@ class TeaCacheSession:
 
         self.current_step = initial_step
         self.call_index = 0
-        self.residual = [None]
-        self.previous = [None]
-        self.distance = [0.0]
-        self.consecutive_hits = [0]
+        self.residual: list[Optional[th.Tensor]] = [None]
+        self.previous: Optional[th.Tensor] = None
+        self.distance = 0.0
+        self.consecutive_hits = 0
+        self.use_cache = True
 
     def next_step(self):
         self.current_step += 1
         self.call_index = 0
+        self.use_cache = True
 
 
 class TeaCacheScript(scripts.Script):
@@ -155,14 +157,8 @@ def patched_forward(
     # cache multiple model calls in one step
     if index == len(_cache.residual):
         _cache.residual.append(None)
-        _cache.previous.append(None)
-        _cache.distance.append(0.0)
-        _cache.consecutive_hits.append(0)
 
-    previous = _cache.previous[index]
     residual = _cache.residual[index]
-    distance = _cache.distance[index]
-    consecutive_hits = _cache.consecutive_hits[index]
 
     assert (y is not None) == (
         self.num_classes is not None
@@ -175,8 +171,6 @@ def patched_forward(
         assert y.shape[0] == x.shape[0]
         emb = emb + self.label_emb(y)
 
-    use_cache = True
-    first_block_residual = None
     h = x
     for id, module in enumerate(self.input_blocks[:2]):
         if id == 1:
@@ -184,33 +178,36 @@ def patched_forward(
         h = module(h, emb, context)
         hs.append(h)
 
-        # check cache
-        if id == 1:  # id=0 is only conv
+        # check cache condition on first model call only
+        if id == 1 and index == 0:  # id=0 is only conv
             first_block_residual = h - original_h
 
             # cache conditions
             # check step range
             progress = _cache.current_step / _cache.steps
             if not (_cache.start < progress <= _cache.end):
-                use_cache = False
+                _cache.use_cache = False
             # check max consecutive cache hits
-            if _cache.max_consecutive > 0 and consecutive_hits >= _cache.max_consecutive:
-                use_cache = False
+            if _cache.max_consecutive > 0 and _cache.consecutive_hits >= _cache.max_consecutive:
+                _cache.use_cache = False
             # check cached value exists
-            if previous is None or residual is None:
-                use_cache = False
+            if _cache.previous is None or residual is None:
+                _cache.use_cache = False
 
-    if use_cache:
-        p = Polynomial([-7.33293101e-02, 5.57968864e+00, -6.85753651e+01, 4.04814722e+02, -8.11904600e+02])  # NoobAI XL vpred v1.0
-        # p = Polynomial([-4.46619183e-02,  2.04088614e+00, -1.30308644e+01,  1.01387815e+02, -2.48935677e+02])  # NoobAI XL v1.1
-        distance += p(relative_l1_distance(previous, first_block_residual)).item()
-        if distance >= _cache.threshold:
-            use_cache = False
-    previous = first_block_residual
+            # cache indicator
+            if _cache.use_cache:
+                p = Polynomial([-7.33293101e-02, 5.57968864e+00, -6.85753651e+01, 4.04814722e+02, -8.11904600e+02])  # NoobAI XL vpred v1.0
+                # p = Polynomial([-4.46619183e-02,  2.04088614e+00, -1.30308644e+01,  1.01387815e+02, -2.48935677e+02])  # NoobAI XL v1.1
+                _cache.distance += p(relative_l1_distance(_cache.previous, first_block_residual)).item()
+                if _cache.distance >= _cache.threshold:
+                    _cache.use_cache = False
+                    _cache.distance = 0.0
+                else:
+                    _cache.consecutive_hits += 1
+            _cache.previous = first_block_residual
 
-    if use_cache:
+    if _cache.use_cache:
         h += residual
-        consecutive_hits += 1
     else:
         original_h = h
         for module in self.input_blocks[2:]:
@@ -222,13 +219,8 @@ def patched_forward(
             h = module(h, emb, context)
 
         residual = h - original_h
-        distance = 0.0
-        consecutive_hits = 0
 
     _cache.residual[index] = residual
-    _cache.previous[index] = previous
-    _cache.distance[index] = distance
-    _cache.consecutive_hits[index] = consecutive_hits
     _cache.call_index += 1
 
     h = h.type(x.dtype)
